e89c21a7f31cfaabe199ebad788def43d62963a6
Alex <aleksandrekiknadzee@gmail.com>
2024-09-15 14:25:07 +0400

    config: generate tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
-
 // https://vitejs.dev/config/
 export default defineConfig({
+	css: {
+		postcss: {
+			plugins: [tailwindcss()],
+		}
+	},
 	plugins: [react()],
 	resolve: {
 		alias: {
