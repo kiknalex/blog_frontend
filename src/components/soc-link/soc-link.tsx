7446fb0890f6e322fef4788846ada446aa0bfbe4
Alex <aleksandrekiknadzee@gmail.com>
2024-09-15 19:14:42 +0400

    refactor: remove target prop from social links

--- src/components/soc-link/soc-link.tsx
+++ src/components/soc-link/soc-link.tsx
@@ -4,17 +4,20 @@ import linkedinIconUrl from "@assets/linkedin-brands-solid.svg";
 const SocLink = ({
 	className,
 	socMedia,
-	target,
 	url,
 }: {
 	className: string;
 	socMedia: "github" | "linkedin";
-	target?: "_blank";
 	url: string;
 }) => {
 	const iconUrl = socMedia === "github" ? githubIconUrl : linkedinIconUrl;
 	return (
-		<a className="inline-block p-2" href={url} target={target}>
+		<a
+			className="inline-block p-2"
+			href={url}
+			rel="noopener noreferrer"
+			target="_blank"
+		>
 			<img
 				alt={`${socMedia} icon`}
 				className={className}
--- src/widgets/soc-media-icons-row/soc-media-icons-row.tsx
+++ src/widgets/soc-media-icons-row/soc-media-icons-row.tsx
@@ -6,13 +6,11 @@ const SocMediaIconsRow = () => {
 			<SocLink
 				className="w-6"
 				socMedia="github"
-				target="_blank"
 				url="https://github.com/kiknalex/blog_frontend"
 			/>
 			<SocLink
 				className="w-6"
 				socMedia="linkedin"
-				target="_blank"
 				url="https://www.linkedin.com/in/aleksandre-kiknadze-a94272221/"
 			/>
 		</div>
