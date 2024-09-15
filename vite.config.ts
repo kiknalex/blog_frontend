1d35d64b85c013843a14c190455e4ef198843f7f
Alex <aleksandrekiknadzee@gmail.com>
2024-09-15 13:08:47 +0400

    config: add new absolute import path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": "/src",
+			"@assets": "/src/assets",
+		},
+	},
+});
