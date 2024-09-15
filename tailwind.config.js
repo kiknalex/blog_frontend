1fabeac210bbc83ad6b0b7beea7f42773c50a937
Alex <aleksandrekiknadzee@gmail.com>
2024-09-15 15:05:30 +0400

    feat: add default styles to container

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
+		container: {
+			center: true,
+			inlinePadding: "20px",
+		},
 	},
 	plugins: [],
 };
