cf39350df23206dc49254c308d40f928030eb48f
Alex <aleksandrekiknadzee@gmail.com>
2024-09-15 19:07:00 +0400

    feat: add footer

--- src/widgets/footer/footer.tsx
+++ src/widgets/footer/footer.tsx
@@ -1,5 +1,16 @@
+import SocMediaIconsRow from "../soc-media-icons-row/soc-media-icons-row";
+
 const Footer = () => {
-	return <footer>This is footer.</footer>;
+	return (
+		<footer className="container flex h-20 items-center justify-between">
+			<div>
+				<p className="font-bold">
+					hotcoffee <small>2024 &#169; all rights reserved</small>
+				</p>
+			</div>
+			<SocMediaIconsRow />
+		</footer>
+	);
 };
 
 export default Footer;
