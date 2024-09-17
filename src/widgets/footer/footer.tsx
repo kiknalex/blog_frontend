ed9caa3f5c38ac2c797aa32329df07b5f50783d2
Alex <aleksandrekiknadzee@gmail.com>
2024-09-17 12:11:03 +0400

    feat: add border to footer

--- src/widgets/footer/footer.tsx
+++ src/widgets/footer/footer.tsx
@@ -2,13 +2,15 @@ import SocMediaIconsRow from "../soc-media-icons-row/soc-media-icons-row";
 
 const Footer = () => {
 	return (
-		<footer className="container flex h-20 items-center justify-between">
-			<div>
-				<p className="font-bold">
-					hotcoffee <small>2024 &#169; all rights reserved</small>
-				</p>
+		<footer className=" flex h-20 items-center divide-gray-300 border-t-2 border-solid">
+			<div className="container flex items-center justify-between">
+				<div>
+					<p className="font-bold">
+						hotcoffee <small>2024 &#169; all rights reserved</small>
+					</p>
+				</div>
+				<SocMediaIconsRow />
 			</div>
-			<SocMediaIconsRow />
 		</footer>
 	);
 };
