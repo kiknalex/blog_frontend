4660aa98b30a2ecaf28e888034b5746cefafa246
Alex <aleksandrekiknadzee@gmail.com>
2024-09-17 18:18:05 +0400

    feat: add hover to nav links

--- src/widgets/header/nav/nav.tsx
+++ src/widgets/header/nav/nav.tsx
@@ -1,13 +1,13 @@
 const Nav = () => {
 	return (
 		<nav className="flex gap-1">
-			<a className="px-1" href="/">
+			<a className="px-1 hover:text-yellow-500" href="/">
 				Home
 			</a>
-			<a className="px-1" href="/posts">
+			<a className="px-1 hover:text-yellow-500" href="/posts">
 				Posts
 			</a>
-			<a className="px-1" href="login">
+			<a className="px-1 hover:text-yellow-500" href="login">
 				Login
 			</a>
 		</nav>
