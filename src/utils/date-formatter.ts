7f5ce5bc42b10fe8179cdea79ba55c7c67e08666
Alex <aleksandrekiknadzee@gmail.com>
2024-09-19 18:19:05 +0400

    feat: add date formatter for comment

--- src/utils/date-formatter.ts
+++ src/utils/date-formatter.ts
@@ -3,3 +3,11 @@ export const dateFormatter = new Intl.DateTimeFormat("en-US", {
 	month: "short",
 	year: "numeric",
 });
+
+export const dateWithTimeFormatter = new Intl.DateTimeFormat("en-US", {
+	day: "numeric",
+	month: "short",
+	year: "numeric",
+	hour: "numeric", //eslint-disable-line perfectionist/sort-objects
+	minute: "numeric",
+});
