8df3736c7a79c0d43c60080f7080d8d2499d2135
Alex <aleksandrekiknadzee@gmail.com>
2024-09-27 18:43:20 +0400

    feat: add new post add layout

--- src/app/routes/post-new/index.tsx
+++ src/app/routes/post-new/index.tsx
@@ -1,7 +1,48 @@
+import { Form } from "react-router-dom";
+
 const PostNew = () => {
 	return (
 		<main>
-			<h2>New post here.</h2>
+			<Form className="container mt-12">
+				<div className="mx-auto flex max-w-[800px]  flex-col rounded-lg border border-gray-300 p-4 text-gray-800 shadow-lg">
+					<label
+						className="mb-2 block text-sm font-medium text-gray-900 "
+						htmlFor="title"
+					>
+						Title
+					</label>
+					<input
+						id="title"
+						className="block w-full rounded-lg border border-gray-300  p-2 text-base text-gray-900 "
+						maxLength={300}
+						minLength={3}
+						name="title"
+						placeholder="Enter your title..."
+						required
+						type="text"
+					/>
+					<label
+						className="my-2 block text-sm font-medium text-gray-900 "
+						htmlFor="content"
+					>
+						Content
+					</label>
+					<textarea
+						id="content"
+						className="block w-full rounded-lg border border-gray-300  p-2.5  text-gray-900 "
+						maxLength={1000}
+						minLength={50}
+						name="content"
+						placeholder="Enter your content..."
+						required
+						rows={8}
+					></textarea>
+
+					<button className="mt-4 w-20 items-center rounded-lg bg-yellow-300 px-4 py-2.5 text-center text-xs font-medium text-black  hover:bg-yellow-400 focus:ring-1 focus:ring-yellow-400">
+						Submit
+					</button>
+				</div>
+			</Form>
 		</main>
 	);
 };
